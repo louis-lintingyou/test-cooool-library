2b1a3a1eb2fd596aa39b44cb548b73a485bfb5f1
louis-lin <louislin@ryzo.io>
2021-09-07 15:05:54 +0800

    test branch 02

--- src/lib/coooool-library.module.ts
+++ src/lib/coooool-library.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LibCptComponent } from './lib-cpt/lib-cpt.component';
+import { LibCpt3Component } from './lib-cpt3/lib-cpt3.component';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
-    LibCptComponent
+    LibCptComponent,
+    LibCpt3Component
   ],
 })
 export class CooooolLibraryModule {}
---
+++ src/lib/lib-cpt3/lib-cpt3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'library-test-lib-cpt3',
+  templateUrl: './lib-cpt3.component.html',
+  styleUrls: ['./lib-cpt3.component.scss']
+})
+export class LibCpt3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
